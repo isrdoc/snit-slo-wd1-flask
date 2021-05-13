4511cdba7cbbc4d8422e41ed388593f63691e3aa
Igor Srdoc <igor@srdoc.si>
2021-05-13 16:59:24 +0200

    Add content up to database lesson

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
+from sqla_wrapper import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy("sqlite:///db.sqlite")
 
 
-@app.context_processor
-def add_global_variables():
-    return dict(surname="Novak")
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String, unique=False)
+    text = db.Column(db.String, unique=False)
 
 
-@app.route("/")
+db.create_all()
+
+
+@app.route("/", methods=["GET"])
 def index():
+    is_logged_in = request.cookies.get("is_logged_in")
     user_name = "Gregor"
     articles = ["My first article", "It's fun to learn"]
-    is_logged_in = False
     return render_template(
         "index.html",
         name=user_name,
@@ -21,9 +27,46 @@ def index():
     )
 
 
-@app.route("/register")
+@app.route("/chat", methods=["GET", "POST"])
+def chat():
+    if request.method == "GET":
+        messages = db.query(Message).all()
+
+        return render_template("chat.html", messages=messages)
+
+    if request.method == "POST":
+        first_name = request.form.get("first_name")
+        message_text = request.form.get("message_text")
+
+        message = Message(author=first_name, text=message_text)
+        message.save()
+
+        messages = db.query(Message).all()
+
+        return render_template("chat.html", messages=messages)
+
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    if request.method == "GET":
+        return render_template("register.html")
+
+    if request.method == "POST":
+        first_name = request.form.get("first_name")
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        # TODO: Store user to database
+
+        response = make_response(render_template(
+            "register_success.html",
+            first_name=first_name,
+            email=email,
+            is_logged_in=True
+        ))
+        response.set_cookie("is_logged_in", "true")
+
+        return response
 
 
 @app.route("/profile")
