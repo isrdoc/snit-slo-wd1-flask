4de519495d9b98c9e2f2f8b8736eecad510fcf25
Igor Srdoc <igor@srdoc.si>
2021-05-13 18:28:47 +0200

    Add login with email cookie

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, make_response
+from flask import Flask, render_template, request, make_response, redirect, url_for
 from sqla_wrapper import SQLAlchemy
 
 app = Flask(__name__)
@@ -11,19 +11,27 @@ class Message(db.Model):
     text = db.Column(db.String, unique=False)
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, unique=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, unique=False)
+
+
 db.create_all()
 
 
 @app.route("/", methods=["GET"])
 def index():
-    is_logged_in = request.cookies.get("is_logged_in")
-    user_name = "Gregor"
+    email = request.cookies.get("email")
+    user = db.query(User).filter_by(email=email).first()
+
     articles = ["My first article", "It's fun to learn"]
+
     return render_template(
         "index.html",
-        name=user_name,
-        articles=articles,
-        is_logged_in=is_logged_in
+        user=user,
+        articles=articles
     )
 
 
@@ -46,6 +54,28 @@ def chat():
         return render_template("chat.html", messages=messages)
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = db.query(User).filter_by(email=email).first()
+        if not user:
+            return render_template("login.html", did_login_fail=True)
+
+        are_passwords_same = user.password == password
+        if not are_passwords_same:
+            return render_template("login.html", did_login_fail=True)
+
+        response = make_response(redirect(url_for('index')))
+        response.set_cookie("email", email)
+        return response
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "GET":
@@ -56,7 +86,12 @@ def register():
         email = request.form.get("email")
         password = request.form.get("password")
 
-        # TODO: Store user to database
+        # TODO: Check valid email and password
+
+        user = User(first_name=first_name, email=email, password=password)
+
+        db.add(user)
+        db.commit()
 
         response = make_response(render_template(
             "register_success.html",
@@ -64,7 +99,7 @@ def register():
             email=email,
             is_logged_in=True
         ))
-        response.set_cookie("is_logged_in", "true")
+        response.set_cookie("email", email)
 
         return response
 
